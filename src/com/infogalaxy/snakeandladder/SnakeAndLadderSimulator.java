eeab003b5f9c2d26497fab280af9e9840a548c87
khokharmosin <khokharmosin7806@gmail.com>
2022-04-01 11:37:27 +0530

    MODIFIED: Display Die Numbers 1 To 6

--- src/com/infogalaxy/snakeandladder/SnakeAndLadderSimulator.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadderSimulator.java
@@ -8,9 +8,14 @@ public class SnakeAndLadderSimulator {
         int playerposition = 0;
         System.out.println("Player Position Is : "+playerposition);
     }
-
+    public void rollsDie(){
+        Random random = new Random();
+        int rollsDie = (int) ((Math.random()*(6-1))+1);
+        System.out.println("Die Number Is : "+rollsDie);
+    }
     public static void main(String[] args) {
         SnakeAndLadderSimulator player1 = new SnakeAndLadderSimulator();
         player1 . playerposition();
+        player1 . rollsDie();
     }
 }
